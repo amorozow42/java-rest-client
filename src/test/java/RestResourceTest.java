e217cd9a15c8637db19abf53d884ec796c712657
Alexey Morozov <losaciertos@gmail.com>
2021-10-11 18:55:56 +0300

    Add test for single resource get

--- src/test/java/RestResourceTest.java
+++ src/test/java/RestResourceTest.java
@@ -4,8 +4,18 @@ import org.junit.Test;
 public final class RestResourceTest {
 
     @Test
-    public void getResource() {
-        final RestResource resource = new RestResource("https://api.github.com/repos/romero/java-rest-client/issues");
+    public void getResourceList() {
+        final RestResource resource =
+            new RestResource("https://api.github.com/repos/romero/java-rest-client/issues");
+        final String json = resource.get();
+
+        assertFalse(json.isEmpty());
+    }
+
+    @Test
+    public void getSingleResource() {
+        final RestResource resource =
+            new RestResource("https://api.github.com/repos/romero/java-rest-client/issues/1");
         final String json = resource.get();
 
         assertFalse(json.isEmpty());
